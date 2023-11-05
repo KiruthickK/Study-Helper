aca293ba1fc75323ec347df528ed543b80cf38c2
KiruthickK <kiruthick101@gmail.com>
2023-11-05 19:35:31 +0530

    basic node project initiated with mysql

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-import mysql from "mysql2"
-// const pool = 
+import mysql from 'mysql2'
+import express from 'express'
+
+const app = express();
+const server = 'localhost';
+const user_name = 'root';
+const user_pass = 'root';
+const database = 'studyhelper';
+
+const pool = mysql.createPool({
+    host: server,
+    user: user_name,
+    password: user_pass,
+    database: database
+});
+
+app.use(express.static('public'));
+// app.use("/scripts", express.static(__dirname + '/scripts'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+app.listen(3000, () => console.log('Listening...'));
+
+app.get('/', (req, res) => {
+    res.render('index', { page_title: 'StudyHelper' })
+})
