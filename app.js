b9f20545878b75f213d269e5a040bec39acbf9d1
KiruthickK <kiruthick101@gmail.com>
2023-11-08 18:41:55 +0530

    added db part

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-import mysql from 'mysql2'
 import express from 'express'
-import dotenv from 'dotenv'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const app = express();
-// For mysql database connection
-const server = process.env.MYSQL_HOST;
-const user_name = process.env.USER;
-const user_pass = process.env.PASS;
-const database = process.env.DBNAME;
+
 // for getting directory path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-dotenv.config()
 // Creating pool of connection to mysql
-const pool = mysql.createPool({
-    host: server,
-    user: user_name,
-    password: user_pass,
-    database: database
-}).promise()
 
 app.use(express.static('public'));
 app.use("/scripts", express.static(__dirname + '/scripts'));
@@ -36,4 +23,4 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render('login', { page_title: "Login | Signup" })
-})
+});
---
+++ database.js
@@ -0,0 +1,32 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// For mysql database connection
+const server = process.env.MYSQL_HOST;
+const user_name = process.env.USER;
+const user_pass = process.env.PASS;
+const database = process.env.DBNAME;
+
+const pool = mysql.createPool({
+    host: server,
+    user: user_name,
+    password: user_pass,
+    database: database
+}).promise()
+export async function checkUniqueEmail(email) {
+    const [result] = await pool.query(`SELECT name FROM student WHERE email = ?`, [email]);
+    return result.length == 0;
+}
+export async function createUser(name, age, email, roll_no) {
+    const isEmailAlreadyUsed = await checkUniqueEmail(email);
+    if (isEmailAlreadyUsed) {
+        const [result] = await pool.query(`INSERT INTO student (name, age, email, roll_no) values (?, ?, ?, ?)`, [name, age, email, roll_no]);
+        console.log(result);
+    } else {
+        return false;
+    }
+
+}
+// const flag = await checkUniqueEmail('kiruthicdfgk101@gmail.com');
