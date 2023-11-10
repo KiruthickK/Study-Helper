1d43d79eb224714b41962b4e3475c9d164916992
KiruthickK <kiruthick101@gmail.com>
2023-11-10 09:29:47 +0530

    session implemented

--- scripts/coursePage.js
+++ scripts/coursePage.js
@@ -1,3 +1,5 @@
 window.onload = () => {
-
+    const auth_code = sessionStorage.getItem("auth_code");
+    const user_id = sessionStorage.getItem("user_id");
+    console.log(user_id, " : ", auth_code)
 }
